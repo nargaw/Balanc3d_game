c03c4147778e8cd82e0f4def7c2f7b80abed3841
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2024-03-27 15:56:50 -0400

    added confetti component and create level complete ui

---
+++ src/LevelCompleteUI.jsx
@@ -0,0 +1,29 @@
+
+
+export default function LevelCompleteUI()
+{
+    const containerStyle = {
+
+    }
+
+    const titleStyle1 = {
+
+    }
+
+    const buttonContainerStyle = {
+
+    }
+
+
+    return <>
+        <div style={containerStyle}>
+            <p style={titleStyle1}>Level</p>
+            <p>1</p>
+            <p>Complete</p>
+            <div style={buttonContainerStyle}>
+                <button>Replay</button>
+                <button>Next</button>
+            </div>
+        </div>
+    </>
+}
